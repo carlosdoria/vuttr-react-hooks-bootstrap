62e1ae7128fc4f3fdbec691ebf1bf5a1c17d47c6
Carlos Doria <carlosdoria953@gmail.com>
2020-09-11 23:39:38 -0300

    Text exchanged in ModalSucess.

--- src/components/ModalSucess/modalSucess.jsx
+++ src/components/ModalSucess/modalSucess.jsx
@@ -6,16 +6,16 @@ function ModalSucess (props) {
   return(
     props.method === 'delete' ?
       <Modal isOpen={props.visible} modalTransition={{ timeout: 400 }}>
-        <ModalHeader>Sucesso</ModalHeader>
+        <ModalHeader>Sucess</ModalHeader>
         <ModalBody>
           Ferramenta deleteda com sucesso!
         </ModalBody>
       </Modal>
     :
       <Modal isOpen={props.visible} modalTransition={{ timeout: 400 }}>
-        <ModalHeader>Sucesso</ModalHeader>
+        <ModalHeader>Sucess</ModalHeader>
         <ModalBody>
-          Ferramenta cadastrada com sucesso!
+          Tool successfully registered!
         </ModalBody>
       </Modal>
   )
